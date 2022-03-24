24205dfff83e1477902f28a674ac1fd7428c9392
rickdeu <hangaloandre@gmail.com>
2022-03-24 16:17:34 +0100

    first commit

---
+++ core/user/forms.py
@@ -0,0 +1,128 @@
+from django import forms
+from django.forms import ModelForm
+
+from core.user.models import User
+
+
+class UserForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['first_name'].widget.attrs['autofocus'] = True
+
+    class Meta:
+        model = User
+        fields = 'first_name', 'last_name', 'email', 'username', 'password', 'image', 'groups'
+        widgets = {
+            'first_name': forms.TextInput(
+                attrs={
+                    'placeholder': 'Digite seu nome',
+                }
+            ),
+            'last_name': forms.TextInput(
+                attrs={
+                    'placeholder': 'Digite seu sobrenome',
+                }
+            ),
+            'email': forms.TextInput(
+                attrs={
+                    'placeholder': 'Digite seu e-mail',
+                }
+            ),
+            'username': forms.TextInput(
+                attrs={
+                    'placeholder': 'entre com seu nome de usuário',
+                }
+            ),
+            'password': forms.PasswordInput(render_value=True,
+                                            attrs={
+                                                'placeholder': 'Coloque sua senha',
+                                            }
+                                            ),
+            'groups': forms.SelectMultiple(attrs={
+                'class': 'form-control select2',
+                'style': 'width: 100%',
+                'multiple': 'multiple'
+            })
+        }
+        exclude = ['user_permissions', 'last_login', 'date_joined', 'is_superuser', 'is_active', 'is_staff']
+
+    def save(self, commit=True):
+        data = {}
+        form = super()
+        try:
+            if form.is_valid():
+                pwd = self.cleaned_data['password']
+                u = form.save(commit=False)
+                if u.pk is None:
+                    u.set_password(pwd)
+                else:
+                    user = User.objects.get(pk=u.pk)
+                    if user.password != pwd:
+                        u.set_password(pwd)
+                u.save()
+                u.groups.clear()
+                for g in self.cleaned_data['groups']:
+                    u.groups.add(g)
+            else:
+                data['error'] = form.errors
+        except Exception as e:
+            data['error'] = str(e)
+        return data
+
+
+class UserProfileForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['first_name'].widget.attrs['autofocus'] = True
+
+    class Meta:
+        model = User
+        fields = 'first_name', 'last_name', 'email', 'username', 'password', 'image'
+        widgets = {
+            'first_name': forms.TextInput(
+                attrs={
+                    'placeholder': 'Nome',
+                }
+            ),
+            'last_name': forms.TextInput(
+                attrs={
+                    'placeholder': 'Sobrenome',
+                }
+            ),
+            'email': forms.TextInput(
+                attrs={
+                    'placeholder': 'Email',
+                }
+            ),
+            'username': forms.TextInput(
+                attrs={
+                    'placeholder': 'Usuario',
+                }
+            ),
+            'password': forms.PasswordInput(render_value=True,
+                                            attrs={
+                                                'placeholder': 'Senha',
+                                            }
+                                            ),
+        }
+        exclude = ['user_permissions', 'last_login', 'date_joined', 'is_superuser', 'is_active', 'is_staff', 'groups']
+
+    def save(self, commit=True):
+        data = {}
+        form = super()
+        try:
+            if form.is_valid():
+                pwd = self.cleaned_data['password']
+                u = form.save(commit=False)
+                if u.pk is None:
+                    u.set_password(pwd)
+                else:
+                    user = User.objects.get(pk=u.pk)
+                    if user.password != pwd:
+                        u.set_password(pwd)
+                u.save()
+            else:
+                data['error'] = form.errors
+        except Exception as e:
+            data['error'] = str(e)
+        return data
